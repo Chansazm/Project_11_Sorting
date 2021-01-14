9949a4a466312c9f6c79233f418d2ae898d836ef
Chansa Chishimba <chansazm@yahoo.co.uk>
2021-01-13 19:59:22 -0600

    Working mergesort

---
+++ mergeready.py
@@ -0,0 +1,38 @@
+def merge_sort(array):
+
+    if len(array) <= 1:
+        return array
+
+    midpoint = int(len(array) / 2)
+
+    left, right = merge_sort(array[:midpoint]), merge_sort(array[midpoint:])
+
+    return merge(left, right)
+
+
+def merge(left, right):
+
+    result = []
+    left_pointer = right_pointer = 0
+
+    while left_pointer < len(left) and right_pointer < len(right):
+
+        if left[left_pointer] < right[right_pointer]:
+
+            result.append(left[left_pointer])
+            left_pointer += 1
+
+        else:
+
+            result.append(right[right_pointer])
+            right_pointer += 1
+
+    result.extend(left[left_pointer:])
+    result.extend(right[right_pointer:])
+
+    return result
+
+
+
+array = [5, 4, 3, 2, 1]
+print(merge_sort(array))
