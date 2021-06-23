71efb98504cf60aeafcab117512fe9bcf9bc1d27
Chansa Chishimba <chansazm@yahoo.co.uk>
2021-06-23 15:53:26 +0200

    Modified bubblesort

--- bubblesort.py
+++ bubblesort.py
@@ -1,17 +1,12 @@
-# Bubble sort algorithm
-
-
 def bubble_sort(data):
-    # using the array length and then decreamenting
-    for i in range(len(data)-1, 0, -1):
+    for i in range(len(data)-1):
         for j in range(i):
             if data[j] > data[j+1]:
-                temp = data[j]
-                data[j] = data[j+1]
-                data[j+1] = temp
+                data[j], data[j+1] = data[j+1], data[j]
+    return data
 
 
-# Test our bubble sort
+# Driver function
 numbers = [1, 8, 5, 7]
 bubble_sort(numbers)
 print(numbers)
